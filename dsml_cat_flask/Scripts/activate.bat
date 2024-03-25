7eb08eba380cd09f137522271bfdc7bf1d5eaa1d
Sainadh-Bahadursha <107560972+Sainadh-Bahadursha@users.noreply.github.com>
2024-03-25 14:48:37 +0530

    Test predictions is successful

--- dsml_cat_flask/Scripts/activate.bat
+++ dsml_cat_flask/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\saina\Desktop\DS_ML_AI\Scaler\Module_17_MLOPS\Lecture_3_Develop Web APIs using Flask\Git_repo_flask\DSML_CAT_FLASK\dsml_cat_flask
+set VIRTUAL_ENV=C:\Users\saina\Desktop\DS_ML_AI\Scaler\Module_17_MLOPS\Lecture_3_Develop_Web_APIs_using_Flask\Git_repo_flask\DSML_CAT_FLASK\dsml_cat_flask
 
 if not defined PROMPT set PROMPT=$P$G
 
--- test_predictions.py
+++ test_predictions.py
@@ -14,3 +14,15 @@ def test_pinger(client):
     resp = client.get("/ping")
     assert resp.status_code == 200
     assert resp.json == {"MESSAGE": "Hi I am Pinging"}
+
+def test_predictions(client):
+    test_data = {
+    "Gender": "Male",
+    "Married": "Yes",
+    "ApplicantIncome": 500000,
+    "LoanAmount": 5000,
+    "Credit_History": 1.0 
+    }
+    resp = client.post("/predict",json = test_data)
+    assert resp.status_code == 200
+    assert resp.json == {"loan_approval_status":"Rejected"}
